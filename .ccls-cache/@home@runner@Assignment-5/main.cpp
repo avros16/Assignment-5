d8e2178ff74240b3b1d4d534bc64b91011273ffd
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-27 13:09:39 +0000

    problem 2

--- .ccls-cache/@home@runner@Assignment-5/main.cpp
+++ .ccls-cache/@home@runner@Assignment-5/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // Q1 Use a loop to calculate the factorial of an integer 
-//Q2 User a loop to calculate the result of a number raised to an exponent 
+//Q2 Use a loop to calculate the result of a number raised to an exponent 
 //Q3 Take a number(let's say n) from the user, and find first n fibonacci sequences.
 
 using std::cout;
@@ -10,26 +10,60 @@ using std::endl;
 
 int main() {
 
-  int num{0};
-  int count{0};
-  int fact{1};
+
+   //******** problem 1 *******
+//   int num{0};
+//   int count{0};
+//   int fact{1};
 
 
 
   
- cout << "Enter an integer";
-  cin >> num;
+//  cout << "Enter an integer";
+//   cin >> num;
 
   
-while (count < num) {
+// while (count < num) {
   
-  fact = fact * (num - count);
+//   fact = fact * (num - count);
 
-  // factoral = 1 * (5-0) = 1*5 = 5
-  // factorial = 5 * (5-1) = 5*4
+//   // factoral = 1 * (5-0) = 1*5 = 5
+//   // factorial = 5 * (5-1) = 5*4
   
-  count++;
+//   count++;
   
+// }
+//   cout << fact;
+
+//****** Problem 2 ******
+
+
+int num2{0};
+int num{0};
+int count{0};
+int expo{0};
+int calc {1};
+
+
+
+  
+  cout << "Enter an integer";
+   cin >> num; 
+
+  cout << "Enter an exponent";
+  cin >> expo;
+
+  
+while (count <= expo) {
+  
+  num2= (num * num);
+  calc = num2; 
+  count ++;
 }
-  cout << fact;
+  
+cout << calc;
+  
+
+
+  
 } 
